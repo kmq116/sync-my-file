13684bb14d4ab4f9a8e649d09f90c6de3d06418e
飞天御剑流co <521luqiya@gmail.com>
2024-08-05 16:59:21 +0800

    feat: 添加上传本地文件，可以根据 ipv4 和文件地址生成二维码

--- public/script.js
+++ public/script.js
@@ -1,47 +1,69 @@
+
 const socket = io();
+const { createApp, ref } = Vue
 
-const fileList = document.getElementById('file-list');
+createApp({
+  setup() {
 
-socket.on('file-list', (files) => {
-  updateFileList(files);
-});
+    socket.on('file-list', (files) => {
+      updateFileList(files);
+    });
 
-socket.on('file-added', (path) => {
-  addFileToList(path);
-});
+    socket.on('file-added', (path) => {
+      addFileToList(path);
+    });
 
-socket.on('file-changed', (path) => {
-  updateFileInList(path);
-});
+    socket.on('file-changed', (path) => {
+      updateFileInList(path);
+    });
 
-socket.on('file-deleted', (path) => {
-  removeFileFromList(path);
-});
+    socket.on('file-deleted', (path) => {
+      removeFileFromList(path);
+    });
 
-function updateFileList(files) {
-  fileList.innerHTML = '';
-  files.forEach(file => {
-    addFileToList(file);
-  });
-}
+    function updateFileList(files) {
+      fileList.innerHTML = '';
+      files.forEach(file => {
+        addFileToList(file);
+      });
+    }
 
-function addFileToList(file) {
-  const li = document.createElement('li');
-  li.textContent = file;
-  fileList.appendChild(li);
-}
+    function addFileToList(file) {
+      console.log(file);
+      fileList.value.push(file)
+      console.log(fileList);
+      // const li = document.createElement('li');
+      // li.textContent = file;
+      // fileList.appendChild(li);
+    }
 
-function updateFileInList(file) {
-  // 这里可以添加更新文件的逻辑，比如改变颜色等
-  console.log('File updated:', file);
-}
+    function updateFileInList(file) {
+      // 这里可以添加更新文件的逻辑，比如改变颜色等
+      console.log('File updated:', file);
+    }
 
-function removeFileFromList(file) {
-  const items = fileList.getElementsByTagName('li');
-  for (let item of items) {
-    if (item.textContent === file) {
-      fileList.removeChild(item);
-      break;
+    function removeFileFromList(file) {
+      const items = fileList.getElementsByTagName('li');
+      for (let item of items) {
+        if (item.textContent === file) {
+          fileList.removeChild(item);
+          break;
+        }
+      }
+    }
+
+    const fileList = ref([])
+    function showQrCode(item) {
+      axios.get('create-qrcode?text=' + item.fileUrl).then(res => {
+        console.log(res.data.data);
+        item.src = res.data.data.base64
+      })
+    }
+    const message = ref('Hello vue!')
+    return {
+      message,
+      fileList,
+      showQrCode
     }
   }
-}
+}).mount('#app')
--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const socketIo = require('socket.io');
 const chokidar = require('chokidar');
 const path = require('path');
 const fs = require('fs');
-
+const axios = require('axios')
+console.log(axios);
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-
+const QRCode = require('qrcode')
 const os = require('os');
+
 
 function getIPv4Address() {
   const interfaces = os.networkInterfaces();
@@ -50,6 +52,41 @@ watcher
   .on('change', path => io.emit('file-changed', path))
   .on('unlink', path => io.emit('file-deleted', path));
 
+
+
+
+app.get('/create-qrcode', async (req, res) => {
+  const text = req.query.text
+  if (!text) {
+    return res.status(400).send({
+      code: 400,
+      data: 'Missing required parameter: text',
+      msg: '缺字段',
+    })
+  }
+
+  try {
+    const qrCodeBase64 = await QRCode.toDataURL(text, { width: 220, margin: 1 })
+    return res.send({
+      code: 200,
+      data: {
+        base64: qrCodeBase64,
+      },
+      msg: '成功',
+    })
+  }
+  catch (error) {
+    console.error(error)
+    return res.status(500).send({
+      code: 500,
+      data: 'Failed to generate QR code.',
+      msg: '',
+    })
+  }
+})
+
+
+
 const PORT = process.env.PORT || 3000;
 io.on('connection', (socket) => {
   console.log('A user connected');
@@ -58,15 +95,13 @@ io.on('connection', (socket) => {
   fs.readdir(syncDir, (err, files) => {
     if (err) throw err;
     console.log({ files });
-    socket.emit('file-list', files.map(fileName => `http://${ipv4Address}:${PORT}/${fileName}`));
+    socket.emit('file-list', files.map(fileName => ({ fileUrl: `http://${ipv4Address}:${PORT}/${fileName}` })));
   });
 
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
-
-
 
 server.listen(PORT, () => console.log(`Server running on port http://${ipv4Address}:${PORT}`));
 
