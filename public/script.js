5df89d64c9285ee1584f980166aa0622f4a717c9
飞天御剑流co <521luqiya@gmail.com>
2024-08-05 18:27:33 +0800

    feat: 添加删除文件的逻辑

--- public/script.js
+++ public/script.js
@@ -1,70 +1,64 @@
-
 const socket = io();
-const { createApp, ref } = Vue
+const {createApp, ref} = Vue
 
 createApp({
-  setup() {
+    setup() {
 
-    socket.on('file-list', (files) => {
-      updateFileList(files);
-    });
+        socket.on('file-list', (files) => {
+            updateFileList(files);
+        });
 
-    socket.on('file-added', (path) => {
-      addFileToList(path);
-    });
+        socket.on('file-added', (path) => {
+            addFileToList(path);
+        });
 
-    socket.on('file-changed', (path) => {
-      updateFileInList(path);
-    });
+        socket.on('file-changed', (path) => {
+            updateFileInList(path);
+        });
 
-    socket.on('file-deleted', (path) => {
-      removeFileFromList(path);
-    });
+        socket.on('file-deleted', (path) => {
+            removeFileFromList(path);
+        });
 
-    function updateFileList(files) {
-      fileList.innerHTML = '';
-      files.forEach(file => {
-        addFileToList(file);
-      });
-    }
-
-    function addFileToList(file) {
-      console.log(file);
-      fileList.value.push(file)
-      console.log(fileList);
-      // const li = document.createElement('li');
-      // li.textContent = file;
-      // fileList.appendChild(li);
-    }
-
-    function updateFileInList(file) {
-      // 这里可以添加更新文件的逻辑，比如改变颜色等
-      console.log('File updated:', file);
-    }
-
-    function removeFileFromList(file) {
-      const items = fileList.getElementsByTagName('li');
-      for (let item of items) {
-        if (item.textContent === file) {
-          fileList.removeChild(item);
-          break;
+        function updateFileList(files) {
+            console.log(files)
+            files.forEach(file => {
+                addFileToList(file);
+            });
         }
-      }
-    }
 
-    const fileList = ref([])
-    function showQrCode(item) {
-      axios.get('create-qrcode?text=' + item.fileUrl).then(res => {
-        item.showQrCode = !item.showQrCode
-        console.log(res.data.data);
-        item.src = res.data.data.base64
-      })
+        function addFileToList(file) {
+            fileList.value.push(file)
+        }
+
+        function updateFileInList(file) {
+            // 这里可以添加更新文件的逻辑，比如改变颜色等
+            console.log('File updated:', file);
+        }
+
+        function removeFileFromList(file) {
+            const index = fileList.value.findIndex(({fileUrl}) => fileUrl === file.fileUrl)
+            console.log({index})
+            if (index !== -1) {
+                fileList.value.splice(index, 1)
+            }
+        }
+
+        const fileList = ref([])
+
+        function showQrCode(item) {
+            axios.get('create-qrcode?text=' + item.fileUrl).then(res => {
+                item.showQrCode = !item.showQrCode
+                console.log(res.data.data);
+                item.src = res.data.data.base64
+            })
+        }
+
+        const message = ref('Hello vue!')
+        return {
+            message,
+            fileList,
+            showQrCode
+        }
     }
-    const message = ref('Hello vue!')
-    return {
-      message,
-      fileList,
-      showQrCode
-    }
-  }
 }).mount('#app')
--- server.js
+++ server.js
@@ -13,16 +13,16 @@ const os = require('os');
 
 
 function getIPv4Address() {
-  const interfaces = os.networkInterfaces();
-  for (const interfaceName in interfaces) {
-    const interface = interfaces[interfaceName];
-    for (const network of interface) {
-      if (network.family === 'IPv4' && !network.internal) {
-        return network.address;
-      }
+    const interfaces = os.networkInterfaces();
+    for (const interfaceName in interfaces) {
+        const interface = interfaces[interfaceName];
+        for (const network of interface) {
+            if (network.family === 'IPv4' && !network.internal) {
+                return network.address;
+            }
+        }
     }
-  }
-  return null;
+    return null;
 }
 
 const ipv4Address = getIPv4Address();
@@ -35,71 +35,70 @@ app.use(express.static('sync'));
 
 // 确保同步目录存在
 if (!fs.existsSync(syncDir)) {
-  fs.mkdirSync(syncDir);
+    fs.mkdirSync(syncDir);
 }
 
 // 设置文件监视器
 const watcher = chokidar.watch(syncDir, {
-  ignored: /(^|[\/\\])\../, // 忽略点文件
-  persistent: true
+    ignored: /(^|[\/\\])\../, // 忽略点文件
+    persistent: true
 });
 
+const genFilePath = (fPath) => {
+    return   {fileUrl: `http://${ipv4Address}:${PORT}/${path.basename(fPath)}`}
+}
 watcher
-  .on('add', __path => {
-    io.emit('file-added', `http://${ipv4Address}:${PORT}/${path.basename(__path)}`)
-  })
-  .on('change', path => io.emit('file-changed', path))
-  .on('unlink', path => io.emit('file-deleted', path));
-
-
+    .on('add', __path => {
+        io.emit('file-added', genFilePath(__path))
+    })
+    .on('change', path => io.emit('file-changed', genFilePath(path)))
+    .on('unlink', path => io.emit('file-deleted', genFilePath(path)));
 
 
 app.get('/create-qrcode', async (req, res) => {
-  const text = req.query.text
-  if (!text) {
-    return res.status(400).send({
-      code: 400,
-      data: 'Missing required parameter: text',
-      msg: '缺字段',
-    })
-  }
+    const text = req.query.text
+    if (!text) {
+        return res.status(400).send({
+            code: 400,
+            data: 'Missing required parameter: text',
+            msg: '缺字段',
+        })
+    }
 
-  try {
-    const qrCodeBase64 = await QRCode.toDataURL(text, { width: 220, margin: 1 })
-    return res.send({
-      code: 200,
-      data: {
-        base64: qrCodeBase64,
-      },
-      msg: '成功',
-    })
-  }
-  catch (error) {
-    console.error(error)
-    return res.status(500).send({
-      code: 500,
-      data: 'Failed to generate QR code.',
-      msg: '',
-    })
-  }
+    try {
+        const qrCodeBase64 = await QRCode.toDataURL(text, {width: 220, margin: 1})
+        return res.send({
+            code: 200,
+            data: {
+                base64: qrCodeBase64,
+            },
+            msg: '成功',
+        })
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({
+            code: 500,
+            data: 'Failed to generate QR code.',
+            msg: '',
+        })
+    }
 })
-
 
 
 const PORT = process.env.PORT || 8899;
 io.on('connection', (socket) => {
-  console.log('A user connected');
+    console.log('A user connected');
 
-  // 发送当前文件列表
-  fs.readdir(syncDir, (err, files) => {
-    if (err) throw err;
-    console.log({ files });
-    socket.emit('file-list', files.map(fileName => ({ fileUrl: `http://${ipv4Address}:${PORT}/${fileName}` })));
-  });
+    // 发送当前文件列表
+    fs.readdir(syncDir, (err, files) => {
+        if (err) throw err;
+        console.log({files});
+        socket.emit('file-list', files.map(fileName => ({fileUrl: `http://${ipv4Address}:${PORT}/${fileName}`})));
+    });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 server.listen(PORT, () => console.log(`Server running on port http://${ipv4Address}:${PORT}`));
